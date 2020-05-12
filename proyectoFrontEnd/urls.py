3a16a74278d508a51081753ea6ec71ddaa778f91
Dargeo <alejoarve12@gmail.com>
2020-05-12 12:48:59 -0500

    commit 2

--- proyectoFrontEnd/urls.py
+++ proyectoFrontEnd/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from temperature import views as temperature_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', temperature_views.temperature, name='temperature'),
 ]
